b106ee535926bcc01108335efcd4aee93f2ea45a
hiruuuuu526 <Sadiyahiru@gmail.com>
2024-11-28 11:15:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session id",
+SESSION_ID: process.env.SESSION_ID || "n2YRmArR#_d7mbaqemSvJtcN71o_YvDeZ6CAO1OsrvuUsaS-sllA",
 MONGODB: process.env.MONGODB || "mongodb://mongo:kxiwsnJulgvyrBCSUritiDjlpJLLPDYf@junction.proxy.rlwy.net:45045"
 };
 
